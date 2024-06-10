4c612dd793444206016f15b2ca15893fbf913280
raihank43 <raihankusuma5@gmail.com>
2024-06-10 13:39:41 +0700

    feat: zodiac list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,35 @@ import { Button } from "@/components/ui/button";
 import ZodiacList from "@/components/zodiacListComponent";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <section className="">
-        <h1>Zodiac List</h1>
+    <main className="flex min-h-screen p-10 gap-2">
+      <section className="flex flex-col gap-10 p-10 bg-purple-100 rounded-lg w-1/2">
+        <h1 className="text-3xl font-bold text-purple-900">Zodiac List</h1>
         <ZodiacList />
       </section>
 
-      <section>
-        <h1>Image</h1>
-        <Image
-          src="/images/zodiac.jpg"
-          alt="Zodiac"
-          width={500}
-          height={500}
-        />
+      <section className="flex flex-col rounded-lg border-purple-200 border-2 w-1/2 p-10">
+        <h1 className="text-4xl font-bold text-purple-800">
+          What Zodiac Sign Am I?
+        </h1>
+        <p className="text-lg text-purple-700">
+          Enter your birthdate to find out your zodiac sign.
+        </p>
+
+        <div className="flex gap-4">
+          <input
+            type="text"
+            className="p-2 border border-black rounded-lg w-1/2"
+            placeholder="Enter your name"
+          />
+          <input
+            type="date"
+            className="p-2 border border-black rounded-lg w-1/2"
+            placeholder="Enter your birthdate"
+          />
+          <Button className="bg-purple-900 text-white">
+            Find My Zodiac Sign
+          </Button>
+        </div>
       </section>
     </main>
   );
--- src/components/zodiacListComponent.tsx
+++ src/components/zodiacListComponent.tsx
@@ -10,22 +10,86 @@ import {
 
 export default function ZodiacList() {
   return (
-    <Table>
+    <Table className="border-2 bg-white">
       <TableCaption>A list of your recent invoices.</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
+          <TableHead>Tanggal Mulai</TableHead>
+          <TableHead>Tanggal Akhir</TableHead>
+          <TableHead>Zodiac</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         <TableRow>
-          <TableCell className="font-medium">INV001</TableCell>
-          <TableCell>Paid</TableCell>
-          <TableCell>Credit Card</TableCell>
-          <TableCell className="text-right">$250.00</TableCell>
+          <TableCell className="font-medium">21 Maret</TableCell>
+          <TableCell>19 April</TableCell>
+          <TableCell>Aries</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">20 April</TableCell>
+          <TableCell>20 Mei</TableCell>
+          <TableCell>Taurus</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">21 Mei</TableCell>
+          <TableCell>20 Juni</TableCell>
+          <TableCell>Gemini</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">21 Juni</TableCell>
+          <TableCell>22 Juli</TableCell>
+          <TableCell>Cancer</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">23 Juli</TableCell>
+          <TableCell>22 Agustus</TableCell>
+          <TableCell>Leo</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">23 Agustus</TableCell>
+          <TableCell>22 September</TableCell>
+          <TableCell>Virgo</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">23 September</TableCell>
+          <TableCell>22 Oktober</TableCell>
+          <TableCell>Libra</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">23 Oktober</TableCell>
+          <TableCell>21 November</TableCell>
+          <TableCell>Scorpio</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">22 November</TableCell>
+          <TableCell>21 Desember</TableCell>
+          <TableCell>Sagittarius</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">22 Desember</TableCell>
+          <TableCell>19 Januari</TableCell>
+          <TableCell>Capricorn</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">20 Januari</TableCell>
+          <TableCell>18 Februari</TableCell>
+          <TableCell>Aquarius</TableCell>
+        </TableRow>
+
+        <TableRow>
+          <TableCell className="font-medium">19 Februari</TableCell>
+          <TableCell>20 Maret</TableCell>
+          <TableCell>Pisces</TableCell>
         </TableRow>
       </TableBody>
     </Table>
